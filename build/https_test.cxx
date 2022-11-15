1413620573bf71a9a7ed29ac4de611731c43d9b2
Edward Zimmermann <edz@nonmonotonic.net>
2022-11-15 22:56:13 +0100

    evolving AUTOREMOTE plugin

--- build/https_test.cxx
+++ build/https_test.cxx
@@ -1,5 +1,10 @@
 //#define CPPHTTPLIB_OPENSSL_SUPPORT
 
+#include "common.hxx"
+#include "record.hxx"
+#include "doc_conf.hxx"
+#include "autodetect.hxx"
+
 #include <httplib.h>
 #include <iostream>
 
@@ -9,6 +14,65 @@
 #include <functional>
 
 //using namespace std;
+//
+//
+//
+// This is the start of development for a DOCTYPE plug-in called
+// AUTOREMOTE
+//
+// It is like AUTODETECT except it handles both local file paths as
+// well as URLs, passing the object then to AUTODETECT
+//
+//
+#if 0
+static const STRING ThisDoctype("AUTOREMOTE");
+
+
+
+class AUTOREMOTE: public AUTODETECT {
+public:
+   AUTOREMOTE(PIDBOBJ DbParent, const STRING& Name) : AUTODETECT(DbParent, Name) {
+      DocumentRoot = DOCTYPE::Getoption("BASE", NulString);
+      if (DocumentRoot.IsEmpty())
+	DocumentRoot = DbParent->ComposeDbFn(".cat");
+
+   };
+
+   const char *Description(PSTRLIST List) const {
+      if ( List->IsEmpty() && !(Doctype ^= ThisDoctype))
+	List->AddEntry(Doctype);
+      List->AddEntry (ThisDoctype);
+      DOCTYPE::Description(List);
+      return "Automatic Remote/Local file indexing Plugin (AUTODETECT)";
+   }
+
+   void ParseRecords(const RECORD& FileRecord) {
+     const STRING s (FileRecord.GetFullFileName());
+     if (s.Search("://") == 0) {
+	// Not URL
+	AUTODETECT::ParseRecords(FileRecord);
+	return;
+     }
+     // Now we need to fetch and put in DocumentRoot
+     RECORD r (FileRecord);
+     std::string   output;
+
+     if (fetch(s.c_str(), &output))
+       {
+        // Got it
+	r.SetDocumentType("AUTODETECT");
+	r.SetFullFileName( output.c_str()); // Set its new path
+	AUTODETECT::ParseRecords(r);
+       }
+     }
+
+   ~AUTOREMOTE() { }
+private:
+   STRING        DocumentRoot;
+
+};
+#endif
+
 
 struct Uri
 {
@@ -82,6 +146,54 @@ static Uri Parse(const std::string &uri)
 };  // uri
 
 
+
+
+static int do_mkdir(const char *path, mode_t mode)
+{
+    struct stat     st;
+    int             status = 0;
+
+    if (stat(path, &st) != 0)
+    {
+        if (mkdir(path, mode) != 0 && errno != EEXIST)
+            status = -1;
+    }
+    else if (!S_ISDIR(st.st_mode))
+    {
+        errno = ENOTDIR;
+        status = -1;
+    }
+
+    return(status);
+}
+
+static int mkpath(const char *path, mode_t mode)
+{
+    char           *pp;
+    char           *sp;
+    int             status;
+    char           *copypath = strdup(path);
+
+    status = 0;
+    pp = copypath;
+    while (status == 0 && (sp = strchr(pp, '/')) != 0)
+    {
+        if (sp != pp)
+        {
+            /* Neither root nor double slash in path */
+            *sp = '\0';
+            status = do_mkdir(copypath, mode);
+            *sp = '/';
+        }
+        pp = sp + 1;
+    }
+    if (status == 0)
+        status = do_mkdir(path, mode);
+    free(copypath);
+    return (status);
+}
+
+
 static const char *metatags[] = {
   "Content-Type",
   "Digest",
@@ -118,7 +230,9 @@ bool fetch(std::ostream& os, const std::string& site, const std::string& file)
 
 const char *base_path = "/tmp";
 
-bool fetch(const char *url)
+const char *ipfs_gateway = "https://ipfs.io";
+
+bool fetch(const char *url, std::string *output)
 {
    Uri u = Uri::Parse (url);
 
@@ -126,29 +240,50 @@ bool fetch(const char *url)
   
    output_name.append("/");
    output_name.append(u.protocol);
-   if (!u.port.empty() && u.port != "80") {
+   if (!u.port.empty() && (u.protocol == "http" && u.port != "80")) {
      output_name.append("_");
      output_name.append(u.port);
    }
    output_name.append("/");
    output_name.append(u.host);
-   output_name.append("/");
-   if (u.path.empty() || u.path == "/")
-     output_name.append("index.html");
-   else 
-     output_name.append(u.path);
 
+   std::string site;
+
+   // IPFS is a special case which we rewrite to go through
+   // a gateway to fetch the resource BUT store on the disk
+   // as IPFS.
+   if (u.protocol == "ipfs") {
+      site = ipfs_gateway;
+      u.path = "/ipfs/";
+      u.path.append(u.host);
+
+      std::string depot = base_path;
+      depot.append("/ipfs");
+      if (mkpath(depot.c_str(), 0777) != 0)
+	return false;
+   } else {
+      site = u.protocol;
+     // make sure we can write to the path
+      if (mkpath(output_name.c_str(), 0777) != 0)
+	return false; // could not create path
+      output_name.append("/");
+      if (u.path.empty() || u.path == "/")
+       output_name.append("_._.html"); // default instead of index.html as they may differ
+      else
+       output_name.append(u.path);
+      if (!site.empty()) site.append("://");
+      site.append(u.host);
+       if (!u.port.empty()) {
+         site.append(":");
+         site.append(u.port);
+       }
+   }
 
    std::cerr << "Output rewrite = " << output_name << std::endl;
-   
-   std::string site = u.protocol;
-   if (!site.empty()) site.append("://");
-   site.append(u.host);
-   if (!u.port.empty()) {
-     site.append(":");
-     site.append(u.port);
-   }
-  std::cerr << "Fetching site: " << site << std::endl;
+
+   if (output) *output = output_name;
+
+  std::cerr << "Fetching site: " << site << "  into " << output_name << std::endl;
   std::cerr  << "Path: " << u.path << std::endl;
 
   std::ofstream ofs;
@@ -159,7 +294,9 @@ bool fetch(const char *url)
 int main(int argc, char **argv)
 {
   if (argc > 1) {
-    fetch( argv[1] );
+    std::string out;
+    bool result = fetch( argv[1], &out );
+    cout << "Result: " << result << " --> " << out << endl;
   }
   else std::cerr << "Usage " << argv[0] << ": URL" << std::endl;
   return 0;
